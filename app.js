deec0d8fae0cfca2884a2e726f9439093c2733a7
lucasfasanella00 <edittryhard@outlook.com>
2024-01-24 21:10:32 -0300

    form register finish

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
 app.use(express.static('public'));
 
 app.listen(port, () => console.log(`http://localhost:${port}`))
